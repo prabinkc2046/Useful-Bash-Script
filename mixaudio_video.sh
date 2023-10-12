298c8d2a25b48fac14af5a8bf25d55c1b4ab464d
Prabin K C <pkmiracle36@gmail.com>
2023-10-12 17:34:21 +1100

    adding a script that automate mixing audio and video

---
+++ mixaudio_video.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Check three arguements are provided
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 audio_file_path video_file_path output_file_name"
+    exit 1
+fi
+
+aud=$1
+vid=$2
+output=$3
+
+# Check if the file exits or not
+if [ ! -e "$aud" ]; then
+   echo "$aud file does not exit."
+   exit 2
+fi
+
+# Check if the file exits or not
+if [ ! -e "$vid" ]; then
+   echo "$vid file does not exit."
+   exit 3
+fi
+
+
+/usr/bin/ffmpeg -i "$aud" -i "$vid" -c:v copy -c:a aac -strict experimental "$output.mkv"
